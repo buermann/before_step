17f5cd217e0bc8c9fccbf35f4b01c2b11f7eb0b7
Josh Buermann <buermann@gmail.com>
2010-12-07 21:58:03 -0600

    A BeforeStep hook.

---
+++ features/support/cucumber.rb
@@ -0,0 +1,64 @@
+require 'cucumber'
+#Cucumber::Ast::StepInvocation.class_eval do
+# class_eval loses the Pending exception class that would otherwise be there.
+# I do not fully grok why. 
+module Cucumber
+  module Ast
+    class StepInvocation #:nodoc:
+      include Gherkin::Rubify
+      def invoke(step_mother, options)
+        find_step_match!(step_mother)
+        unless @skip_invoke || options[:dry_run] || @exception || @step_collection.exception
+          begin
+            step_mother.before_step
+            @step_match.invoke(@multiline_arg)
+            step_mother.after_step
+            status!(:passed)
+          rescue Pending => e
+            failed(options, e, false)
+            status!(:pending)
+          rescue Undefined => e
+            failed(options, e, false)
+            status!(:undefined)
+          rescue Cucumber::Ast::Table::Different => e
+            @different_table = e.table
+            failed(options, e, false)
+            status!(:failed)
+          rescue Exception => e
+            failed(options, e, false)
+            status!(:failed)
+          end
+        end
+      end
+    end
+  end
+end
+
+Cucumber::LanguageSupport::LanguageMethods.class_eval do
+  def execute_before_step(step)
+    hooks_for(:before_step, step).each do |hook|
+      invoke(hook, 'BeforeStep', step, false)
+    end
+  end
+end
+
+# ugh, duck-typing a mix-in?  class_eval doesn't have much luck with this either.
+module Cucumber
+  module RbSupport
+    module RbDsl
+      def BeforeStep(*tag_expressions, &proc)
+        RbDsl.register_rb_hook('before_step', tag_expressions, proc)
+      end
+    end
+  end
+end
+extend(Cucumber::RbSupport::RbDsl)
+
+Cucumber::StepMother.class_eval do
+  def before_step #:nodoc:
+    return if options[:dry_run]
+    @programming_languages.each do |programming_language|
+      programming_language.execute_before_step(@current_scenario)
+    end
+  end
+end
